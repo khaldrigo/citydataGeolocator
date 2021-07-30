8cde4294d9be33e36452e1da4c80e3637f070410
Rodrigo Lima <86531099+khaldrigo@users.noreply.github.com>
2021-07-30 07:23:25 -0300

    update geolocator screen (add marker)

--- lib/screens/geolocator_screen.dart
+++ lib/screens/geolocator_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controller/geolocator_controller.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GeolocatorScreen extends StatelessWidget {
   const GeolocatorScreen({Key? key}) : super(key: key);
@@ -9,30 +10,36 @@ class GeolocatorScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.put(GeolocatorController());
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Citydata Geolocator'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.location_history),
-            onPressed: () {
-              controller.watchPosition();
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.add_location),
-            onPressed: () {
-              controller.getPostion();
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Obx(
-          () => Text(
-            'Lat: ${controller.latitude.value} | Lng: ${controller.longitude.value}',
-          ),
+        appBar: AppBar(
+          title: Text('Citydata Geolocator'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.location_history),
+              onPressed: () {
+                controller.watchPosition();
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                controller.addMarker();
+              },
+            ),
+          ],
         ),
-      ),
-    );
+        body: GetBuilder<GeolocatorController>(
+          init: controller,
+          builder: (value) => GoogleMap(
+            mapType: MapType.normal,
+            zoomControlsEnabled: true,
+            initialCameraPosition: CameraPosition(
+              target: controller.position,
+              zoom: 16,
+            ),
+            markers: controller.markers,
+            onMapCreated: controller.onMapCreated,
+            myLocationEnabled: true,
+          ),
+        ));
   }
 }
