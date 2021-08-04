02f9b717eb7fa24026910da3c446c05cce3a22cd
Rodrigo Lima <86531099+khaldrigo@users.noreply.github.com>
2021-08-03 22:42:49 -0300

    Update geolocator_screen.dart

--- lib/screens/geolocator_screen.dart
+++ lib/screens/geolocator_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controller/geolocator_controller.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 class GeolocatorScreen extends StatelessWidget {
   const GeolocatorScreen({Key? key}) : super(key: key);
@@ -9,16 +10,11 @@ class GeolocatorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(GeolocatorController());
+    // OneSignal.shared.init('4659e3fe-f729-4c52-a4c7-49ee04f869d1');
     return Scaffold(
         appBar: AppBar(
           title: Text('Citydata Geolocator'),
           actions: [
-            IconButton(
-              icon: Icon(Icons.location_history),
-              onPressed: () {
-                controller.watchPosition();
-              },
-            ),
             IconButton(
               icon: Icon(Icons.delete),
               onPressed: () {
